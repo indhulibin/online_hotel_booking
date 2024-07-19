b6e92513c9f95d6d0e2d89723a406f754481a8fa
Indhu <indhumohan19@gmail.com>
2024-07-19 12:01:27 +0930

    appsrviceprovider.php modified

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,9 +29,14 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        $page_data = Page::where('id',1)->first();
+        //$page_data = Page::where('id',1)->first();
         $room_data = Room::get();
-        view()->share('global_page_data',$page_data);
+        if (Schema::hasTable('pages')) {
+            $page_data = Page::where('id',1)->first();
+            // Your logic here
+            view()->share('global_page_data',$page_data);
+        }
+        //view()->share('global_page_data',$page_data);
         view()->share('global_room_data',$room_data);
 
     }
